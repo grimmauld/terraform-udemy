9f536cf3faffc1483c0fa0e040e0ffd29fb99d70
rl063200 <richard.lee@cerner.com>
2021-03-16 14:56:27 -0500

    demo-1 completed

---
+++ demo-1/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-09943f9da1f1b7899"
+    us-east-2 = "ami-0bcacfac640850227"
+    us-west-1 = "ami-017329b75bfa6772b"
+    us-west-2 = "ami-076cbb27c223df09a"
+  }
+}
